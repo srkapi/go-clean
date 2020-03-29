fa69ea97e1e9f1fa459d74dabf4f022b1b0ca80c
srkapi <albertowas12@hotmail.com>
2020-03-29 18:58:36 +0200

    firt

---
+++ project/adapter/in/rest/RestUserController.go
@@ -0,0 +1,28 @@
+package controllers
+
+import (
+	"../model"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+
+
+
+
+
+var CreateUser = func(w http.ResponseWriter, r *http.Request) {
+	user := &modelsCommand.CreatedUserDto{}
+	reqBody, err := ioutil.ReadAll(r.Body)
+	log.Printf(string(reqBody))
+	if err != nil {
+		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
+	}
+	json.Unmarshal(reqBody, user)
+	userDomain:=user.ToDomain()
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(userDomain.Id)
+}
---
+++ project/go.mod
@@ -0,0 +1,5 @@
+module goproject
+
+require github.com/gorilla/mux v1.6.2
+
+go 1.13
